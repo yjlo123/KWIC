f0f41303b4170ecd532e14e3cce011f4206ff504
Jiang-hao <haojiang@r-92-102-25-172.comp.nus.edu.sg>
2015-08-20 16:22:47 +0800

    ignore misc.xml

--- src/Filter.java
+++ src/Filter.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 /**
  * Created by haojiang on 20/8/15.
  */
 public class Filter {
+
+    public ArrayList<String> data;
+    public Filter Next = null;
+
+    public Filter(){}
+    public void SetNext(Filter nextFilter){
+        this.Next = nextFilter;
+    }
+    public boolean hasNext(){
+        return this.Next!=null;
+    }
+
 }
